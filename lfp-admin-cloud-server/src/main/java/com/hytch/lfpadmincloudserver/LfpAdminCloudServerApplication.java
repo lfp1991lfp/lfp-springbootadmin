9a7336fb2dce16f49f47d514041d0e90dafdc24e
lfp1991lfp <128ling@163.com>
2018-01-22 09:48:05 +0800

    1.增加eureka-server的安全校验机制(security)

--- lfp-admin-cloud-server/src/main/java/com/hytch/lfpadmincloudserver/LfpAdminCloudServerApplication.java
+++ lfp-admin-cloud-server/src/main/java/com/hytch/lfpadmincloudserver/LfpAdminCloudServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+//能够让eureka发现的客户端，要先启动eureka服务端
 @EnableDiscoveryClient
 @EnableAdminServer
 public class LfpAdminCloudServerApplication {
